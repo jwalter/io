c1795d67b367d6eeec7ab1362f1277a905c8d7ca
Michael Jolley <mike@baldbeardedbuilder.com>
2026-06-08 00:00:41 -0500

    Adding missing files

---
+++ src/mcp/index.ts
@@ -0,0 +1,2 @@
+export { loadMcpConfig, saveMcpConfig, addMcpServer, removeMcpServer, toggleMcpServer } from "./config.js";
+export { initMcpRegistry, getMcpServersForSession, listServers } from "./registry.js";
